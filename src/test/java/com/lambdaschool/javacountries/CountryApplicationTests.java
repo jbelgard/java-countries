67e180b7d14f055612f0502789a134824226f94b
Jason Belgard <belgard.jason@gmail.com>
2019-07-16 10:06:52 -0500

    MVP met?

--- src/test/java/com/lambdaschool/javacountries/CountryApplicationTests.java
+++ src/test/java/com/lambdaschool/javacountries/CountryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.lambdaschool.countries;
+package com.lambdaschool.javacountries;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CountriesApplicationTests {
+public class CountryApplicationTests {
 
     @Test
     public void contextLoads() {
